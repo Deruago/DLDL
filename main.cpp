cedb16f11202ec38badb07b0b34da82d5f6cce2d
Thimo Bohmer <thimo@bohmer.net>
2022-01-01 18:37:54 +0100

    Removed unused code
    This code was not used, and should not be executed.

--- main.cpp
+++ main.cpp
@@ -1,28 +1,7 @@
 #include "DLDL/Argument/Interpreter.h"
-#include "DLDL/Flex/Lexer.h"
-#include "DLDL/Bison/Parser.h"
 
 int main(int argc, const char* argv[])
 {
-	auto lexer = DLDL::lexer::Lexer();
-	auto tokens = lexer.Tokenize("aaaa");
-	for (auto token : tokens)
-	{
-		token->Print();
-	}
-	
-	auto parser = DLDL::parser::Parser();
-	auto tree = parser.Parse("a a a a");
-	if (tree == nullptr)
-	{
-		return -1;
-	}
-	else
-	{
-		
-	}
-	
-	
 	DLDL::argument::Interpreter interpreter(argc, argv);
 	return interpreter.Run();
 }
