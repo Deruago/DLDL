6499c201554883e643f2bc83105759af4950b3ff
Thimo Bohmer <thimo@bohmer.net>
2022-01-01 23:35:14 +0100

    Update header file

--- include/DLDL/Argument/LicenseLibrary.h
+++ include/DLDL/Argument/LicenseLibrary.h
@@ -17,37 +17,15 @@ namespace DLDL::argument
 		std::string licenseName;
 		std::string description;
 		std::string link;
-		
-	public:
-		LicenseLibrary(std::string libraryName_, std::string licenseName_, std::string description_, std::string link_ = "")
-			: libraryName(std::move(libraryName_)), licenseName(std::move(licenseName_)), description(
-				std::move(description_)), link(link_)
-
-		{
-		}
 
 	public:
-		std::string GetIndent(size_t size) const
-		{
-			std::string output;
-			for (auto i = 0; i < size; i++)
-			{
-				output += "\t";
-			}
+		LicenseLibrary(std::string libraryName_, std::string licenseName_, std::string description_,
+					   std::string link_ = "");
 
-			return output;
-		}
+	public:
+		std::string GetIndent(size_t size) const;
 
-		std::string GetLicenseMessage(size_t indent = 0) const
-		{
-			return
-				GetIndent(indent) + "Library: " + libraryName + "\n" +
-				GetIndent(indent) + "License: " + licenseName + "\n" +
-				GetIndent(indent) + "Link: " + link + "\n"
-				"\n" +
-				GetIndent(indent) + "Description:\n" +
-				GetIndent(indent) + description + "\n";
-		}
+		std::string GetLicenseMessage(size_t indent = 0) const;
 	};
 }
 
