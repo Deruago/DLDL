110c1553c2dffe480e21f3e31d148947a654789a
Thimo Bohmer <thimo@bohmer.net>
2022-05-04 15:55:37 +0200

    Regenerated Deamer CC Output

--- DLDL_ASSOCIATIVITY/extern/Flex/lex.DLDL_ASSOCIATIVITY.cpp
+++ DLDL_ASSOCIATIVITY/extern/Flex/lex.DLDL_ASSOCIATIVITY.cpp
@@ -1054,7 +1054,7 @@ do_action:	/* This label is used only to access EOF actions. */
 case 1:
 YY_RULE_SETUP
 #line 47 "./DLDL_ASSOCIATIVITY_lexer.l"
-{  if (local_store && !false || local_store && include_deleted) store(new deamer::external::cpp::lexer::TerminalObject(yyval, yylineno, column, std::size_t(1))); else if (local_store && true) store(new deamer::external::cpp::lexer::TerminalObject("", yylineno, column, std::size_t(1))); else if (local_store && false); else if (!false || include_deleted) DLDL_ASSOCIATIVITYlval.Terminal = new deamer::external::cpp::lexer::TerminalObject(yyval, yylineno, column, std::size_t(1)); else DLDL_ASSOCIATIVITYlval.Terminal = new deamer::external::cpp::lexer::TerminalObject("", yylineno, column, std::size_t(1)); handleColumn(yyval); if ((!local_store && true) || (!local_store && include_deleted)) return (ASSOCIATIVITY); }
+{  if (local_store && !false || local_store && include_deleted) store(new deamer::external::cpp::lexer::TerminalObject(yyval, yylineno, column, std::size_t(1))); else if (local_store && true) store(new deamer::external::cpp::lexer::TerminalObject("", yylineno, column, std::size_t(1))); else if (local_store && false); else if (!false || include_deleted) DLDL_ASSOCIATIVITYlval.Terminal = new deamer::external::cpp::lexer::TerminalObject(yyval, yylineno, column, std::size_t(1)); else if (!false) DLDL_ASSOCIATIVITYlval.Terminal = new deamer::external::cpp::lexer::TerminalObject("", yylineno, column, std::size_t(1)); handleColumn(yyval); if ((!local_store && true) || (!local_store && include_deleted)) return (ASSOCIATIVITY); }
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
